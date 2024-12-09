1f7933ebf0bfb0e3e72c4e337dfa7f2d8b54741a
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-09 19:55:06 +0200

    trying to put down working docker-compose.yaml

--- src/main/java/com/example/dbmigrationliquibase/DbMigrationLiquibaseApplication.java
+++ src/main/java/com/example/dbmigrationliquibase/DbMigrationLiquibaseApplication.java
@@ -1,13 +1,24 @@
 package com.example.dbmigrationliquibase;
 
+import com.example.dbmigrationliquibase.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DbMigrationLiquibaseApplication {
+@RequiredArgsConstructor
+public class DbMigrationLiquibaseApplication implements CommandLineRunner {
+
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DbMigrationLiquibaseApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.userRepository.findAll().forEach(System.out::println);
     }
 
 }
